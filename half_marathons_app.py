4845daba69e2dd951500b738f4a800f39ff4d954
John Doe <lukeschneider7@yahoo.com>
2024-10-04 10:54:06 -0400

    changes to app

--- half_marathons_app.py
+++ half_marathons_app.py
@@ -12,21 +12,68 @@ useragent = json.loads(r.text)['user-agent']
 headers = {'User-Agent': useragent,
            'from': 'vrd9sd@virginia.edu'}
 
-st.text_input("Your City", key="city")
-st.text_input("distance? (5k, 10k, half, marathon)", key="distance")
+st.text_input("Your City (ex. Virginia Beach)", key="city")
+st.text_input("Your State (ex. va)", key="state")
+st.text_input("distance? (5k, 10k, half-marathon, marathon)", key="distance")
 st.text_input("Races within how many miles from you? (25, 50, 100, 200)", key="location")
 
 
 # You can access the value at any point with:
-st.session_state.city
-st.session_state.location
+city = st.session_state.city
+race_distance = st.session_state.distance
+location = st.session_state.location
+state = st.session_state.state
 
+if location:
+    url = f"https://runningintheusa.com/classic/list/within-{location}-miles-of-{city.replace(' ', '%20')}-{state}/upcoming/{race_distance}/miles-between-250/page-1"
+    st.write(url)
+else:
+    st.write("Please enter a valid location distance.")
 
-url = f"https://runningintheusa.com/classic/list/within-{st.session_state.distance}"\
-"-miles-of-virginia%20beach-va/upcoming/half-marathon/miles-between-250/page-1"
 r = requests.get(url, headers=headers)
-
 # Parsing HTML code 
 mysoup = BeautifulSoup(r.text, 'html.parser')
 
-st.write(url)
+# part 1B: function to scrape races from the running in the usa website
+def race_df(url):
+    """ function for returning df w/ race info given a url
+    Args:
+        url: (str) a string of a race 
+    Returns:
+        races: (df) a DataFrame containg race dates, titles, cities
+    """
+
+    r = requests.get(url, headers=headers)
+    mysoup = BeautifulSoup(r.text, 'html.parser')
+ 
+    titles = [x.b for x in mysoup.find_all('td', attrs = {'style': 'text-decoration:inherit;'})]
+    titles = [x.string for i, x in enumerate(titles) if i%2==1]
+
+    rowspan_1_2_cities = [x.b for x in mysoup.find_all('td', attrs={'rowspan':['1', '2']})]
+    cities = rowspan_1_2_cities[2:]
+    cities = [x.string for i,x in enumerate(cities) if i%4==0] #and i > 7]
+
+    dates = [x.string for x in mysoup.find_all('div', attrs = {'style':"font-weight:bold"})]
+    dates = [x for i,x in enumerate(dates) if i>1 and i<= (len(cities) + 1)]
+
+    distances = [x.string for x in mysoup.find_all('div', attrs={'style':"padding-left:10px"}) if x.string and (x.string.endswith('run') or x.string.endswith('relay'))]
+   
+    if len(dates) == len(titles) == len(cities) ==len(distances): # ensure I am getting each record (lenths should be the same)
+        # Part 1D: Makes races df, use indexing to feature out featured listings at top
+        races = pd.DataFrame({
+        'date' : dates[2:], 
+        'race': titles[2:],
+        'city': cities[2:], 
+        'distance': distances[2:]})
+    else:
+        races = -1 
+    return races
+new_df = race_df(url)
+
+# Part 1C: Turn this web-scraper into a spider!
+for i in range(2, 7):
+    url = url[:-1] + str(i) # Insert new number into url str for pages 2-6
+    new_df = pd.concat([new_df, race_df(url)], ignore_index=True) # use scraping function as spider
+
+
+st.table(new_df)
