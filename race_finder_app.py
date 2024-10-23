9d2d77b6322ca68bc42650c6fe39d6f18115894a
John Doe <lukeschneider7@yahoo.com>
2024-10-23 11:38:49 -0400

    changes

--- race_finder_app.py
+++ race_finder_app.py
@@ -55,7 +55,7 @@ def race_df(url):
     titles = [x.string for i, x in enumerate(titles) if i%2==1]
 
     rowspan_1_2_cities = [x.b for x in mysoup.find_all('td', attrs={'rowspan':['1', '2']})]
-    cities = rowspan_1_2_cities[:]
+    cities = rowspan_1_2_cities
     cities = [x.string for i,x in enumerate(cities) if i%4==0] #and i > 7]
 
     dates = [x.string for x in mysoup.find_all('div', attrs = {'style':"font-weight:bold"})]
